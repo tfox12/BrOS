fc0f598dca9173a3c1c3f2ec805c6f9f922cc68b
Tim Fox <Tfox12@kent.edu>
2012-10-10 01:39:37 -0400

    So I added a keyboard interrupt. It only does something on the key press. This is done by testing the scan code to see if it is above 0x80. added an ascii table so that we can convert the scan code into the ascii character on screen. When a key is pressed it will print that key to the current cursor position, then advance the cursor to the next position

--- boottest.asm
+++ boottest.asm
@@ -1,5 +1,21 @@
 org 0x7c00
 
+
+;--------------------------------------------------;
+; Set up interrupts
+;--------------------------------------------------;
+cli
+
+mov bx, 0x09 ; interrupt 0x09 is keyboard access
+shl bx, 2    ; multiply by 4, 4 bytes per idt entry
+xor ax, ax
+mov gs, ax
+mov ds, ax
+mov [gs:bx], WORD keyboard_interrupt_routine
+mov [gs:bx+2], ds
+
+sti
+
 ;--------------------------------------------------;
 ; Set print information
 ;--------------------------------------------------;
@@ -10,8 +26,6 @@ mov ah, 0xff
 call clear_screen
 
 mov DWORD [cursor], 0
-mov ax, 0x1120
-call print_char_to_cursor
 
 jmp $
 
@@ -92,7 +106,7 @@ ret
 ;--------------------------------------------------;    
 clear_screen:
     
-    mov ah, [background_color]      ; set the clear color
+    mov ah, 0xff                    ; set the clear color
     mov al, 0x20                    ; the space character
     mov DWORD [cursor], 0            ; set the cursor to 0,0
     mov cx, 2000                    ; we wanna run 2000 times
@@ -104,11 +118,79 @@ clear_screen_loop:
 
 ret
 
+;--------------------------------------------------;
+; Print Hex Value
+;   prints a hex representation of byte in al
+;--------------------------------------------------;
+print_hex_at_cursor:
+    
+    push ax
+    push bx
+    push cx
+
+    mov ch, al
+    mov cl, al
+    and ch, 0xf0
+    and cl, 0x0f
+    rol ch, 4
+    
+    mov ah, 0xf1
+
+    movzx bx, ch
+    mov al, BYTE [hextable + bx]
+    call print_char_to_cursor
+    call advance_cursor
+
+    movzx bx, cl
+    mov al, BYTE [hextable + bx]
+    call print_char_to_cursor
+    call advance_cursor
+
+    pop cx
+    pop bx
+    pop ax
+
+ret
+    
+
+;--------------------------------------------------;    
+; Keyboard Interrupt
+;--------------------------------------------------;    
+keyboard_interrupt_routine:
+
+    in al, 0x60
+    test al, 0x80
+    jz keyboard_interrupt_routine_keydown
+    jmp keyboard_interrupt_routine_keyup
+
+keyboard_interrupt_routine_keydown:
+
+    movzx bx, al
+    mov al, BYTE [lowercase_keys + bx]
+    mov ah, 0xf1
+    call print_char_to_cursor
+    call advance_cursor
+
+keyboard_interrupt_routine_keyup:
+
+keyboard_interrupt_routine_done:
+    
+    mov ax, 0x20
+    out 0x20, ax
+
+iret
+
+
 ;-------------------------------------------------;
 ; System Variables
 ;-------------------------------------------------;
+
 cursor dw 0
-background_color db 0xff
+
+lowercase_keys db 0x00,0x00,'1','2','3','4','5','6','7','8','9','0','-','=',0x00,0x00,'q','w','e','r','t','y','u','i','o','p','[',']',0x00,0x00,'a','s','d','f','g','h','j','k','l',0x3b,0x27,0x00,0x00,0x5c,'z','x','c','v','b','n','m',0x02c,'.','/'
+
+
+hextable db '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
 
 times 510-($-$$) db 0
 
